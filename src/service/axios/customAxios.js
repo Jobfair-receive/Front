1dad667b2199a6466303b5d7dcf0569d68893ab8
원설아 <seolalny@gmail.com>
2023-10-23 00:14:06 +0900

    CHORE :: url 수정

--- src/service/axios/customAxios.js
+++ src/service/axios/customAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const customAxios = axios.create({
-  baseURL: "https://port-0-acon-backend-euegqv2blnyn4a7i.sel5.cloudtype.app/",
+  baseURL: "https://port-0-acon-backend-euegqv2blnyn4a7i.sel5.cloudtype.app",
   withCredentials: true,
 });
 
