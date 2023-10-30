5209a3eec27b9481429fef3332b8b986c4569d56
원설아 <seolalny@gmail.com>
2023-10-30 10:24:39 +0900

    CHORE :: URL 수정

--- src/service/axios/customAxios.js
+++ src/service/axios/customAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const customAxios = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://port-0-acon-backend-euegqv2blnyn4a7i.sel5.cloudtype.app",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
